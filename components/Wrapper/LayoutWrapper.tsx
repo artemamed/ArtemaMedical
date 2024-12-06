24ab796631d612ee8371770a359c717b51cfe56b
Your Username <ubaidullah2305@gmail.com>
2024-12-06 10:12:17 +0500

    set

--- components/Wrapper/LayoutWrapper.tsx
+++ components/Wrapper/LayoutWrapper.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 const LayoutWrapper: NextPage<Props> = ({ children, className }) => {
   return (
-    <div className={` md:container mx-auto px-2 ${className}`}>
+    <div className={` md:container mx-auto px-5 ${className}`}>
       {children}
     </div>
   );
