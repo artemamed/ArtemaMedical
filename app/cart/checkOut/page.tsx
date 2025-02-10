9fb2d5021b7d90c493bb5075fc9746bdca399ba6
Huzaifa Majeed <zaifali785@gmail.com>
2025-02-10 17:14:49 +0500

    0.25 dmmy amount added

--- app/cart/checkOut/page.tsx
+++ app/cart/checkOut/page.tsx
@@ -234,7 +234,7 @@ const CheckOut: React.FC = () => {
     try {
       const apiResponse = await axios.post("/api/jc-p/create-session", {
         // amount: Number(amount),
-        amount: 1,
+        amount: 0.25,
         currency: selectedCurrency,
       });
       const data = await apiResponse.data;
